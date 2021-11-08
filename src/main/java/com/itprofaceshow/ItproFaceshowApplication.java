3a38d37ccb5ec7a35d6383225ee6fdeed278ed7f
vyphotphet100 <caovy2001@gmail.com>
2021-11-08 23:45:18 +0700

    first commit

---
+++ src/main/java/com/itprofaceshow/ItproFaceshowApplication.java
@@ -0,0 +1,13 @@
+package com.itprofaceshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItproFaceshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItproFaceshowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itprofaceshow/ItproFaceshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itprofaceshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItproFaceshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
